8687ad55a38ac01990838f91ddde4772ef2ff996
Fernando <fernandoasfj@gmail.com>
2015-08-12 23:26:24 -0300

    Add and list pokemons

--- app/app.rb
+++ app/app.rb
@@ -1,5 +1,28 @@
-require 'sinatra'
-
 get '/' do
   'fun fun'
+end
+
+get '/pokemon' do
+  form = '<form action="/pokemon" method="post">
+            Name: <input type="text" name="name">
+            <br/>
+            Type: <input type="text" name="type">
+            <br/>
+            <input type="submit">
+        </form>'
+
+  erb form
+end
+
+post '/pokemon' do
+  new_pokemon = Pokemon.new(params)
+  new_pokemon.save
+  redirect to('/pokedex')
+end
+
+get '/pokedex' do
+  list = Pokemon.all.map do |pokemon|
+           "Name: #{pokemon.name} Type: #{pokemon.type}"
+         end.join '<br/>'
+  erb list
 end
---
+++ app/model/pokemon.rb
@@ -0,0 +1,5 @@
+class Pokemon
+  include Mongoid::Document
+  field :name, type: String
+  field :type, type: String
+end
